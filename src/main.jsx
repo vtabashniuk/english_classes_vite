0635adc00a463b1841d119efb73d4fcfe9d59903
Volodymyr Tabashniuk <60541362+vtabashniuk@users.noreply.github.com>
2026-08-16 17:30:53 +0300

    fix bugs 2 with speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "normalize.css";
 import "./index.css";
 import "./i18n";
